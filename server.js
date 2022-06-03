e1ae7eff3cb544afb39cb3c4171844c9a56bf5a4
AtYourServas <joeservas@gmail.com>
2022-06-02 21:24:41 -0400

    adding cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const { request, response } = require('express');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const PORT = 8000;
 
